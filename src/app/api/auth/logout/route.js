b24f99301e8ded803c48a1bb80a15f2932f00c8b
Mohammed Sajidul Islam <sajidislam729@gmail.com>
2024-01-30 17:04:22 +0600

    s

--- src/app/api/auth/logout/route.js
+++ src/app/api/auth/logout/route.js
@@ -1,14 +1,19 @@
 import db from "@/lib/db"
 import { NextResponse } from "next/server"
+import { cookies } from 'next/headers'
 
 export async function GET() {
     try {
         await db.connect()
+        cookies().delete('token')
         const response = NextResponse.json({
             message: 'Logout Successful!',
             success: true,
         })
-        response.cookies.set('token', '')
+        response.cookies.set('token', '', {
+            httpOnly: true, 
+            expires: new Date(0)
+        })
         return response
     } catch (error) {
         return NextResponse.json({ error: error.message }, { status: 400 })
